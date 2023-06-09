72f83b1644580d5287299ffe0db16811edd7cb80
jemba narcisse <cabliveer@gmail.com>
2023-06-09 02:57:24 +0200

    Remove comments lines

--- src/global.js
+++ src/global.js
@@ -1,9 +1,3 @@
-// const URL_BASE = 'https://api.api-ninjas.com/v1/quotes?category';
-
-// export const API_KEY = 'SsiJ6uRp/AITXCHTNPvZXA==MFTXGVpMpYgmVD7n';
-
-// export default URL_BASE;
-
 const database = [
   {
     id: 1,
